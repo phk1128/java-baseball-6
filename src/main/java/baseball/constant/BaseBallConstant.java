95f22b55a8c9ae2f114d44143719ebd0a53526ee
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-09-19 16:10:22 +0900

    feat: 상수화

---
+++ src/main/java/baseball/constant/BaseBallConstant.java
@@ -0,0 +1,14 @@
+package baseball.constant;
+
+public class BaseBallConstant {
+
+	private BaseBallConstant() {
+
+	}
+
+	public static final int MAX_SIZE = 3;
+	public static final int MAX_RANGE = 9;
+	public static final int MIN_RANGE = 1;
+	public static final String RESTART = "1";
+	public static final String EXIT = "2";
+}
