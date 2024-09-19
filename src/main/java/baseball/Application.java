5e9dc78083508764c381dc6b304af5dcf3ec219a
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-09-19 16:21:08 +0900

    feat: 어플리케이션

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import baseball.controller.BaseBallController;
+import baseball.service.BaseBallService;
+import baseball.util.PlayerNumbersValidator;
+import baseball.util.RandomNumbersGenerator;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+
+		final BaseBallService baseBallService = new BaseBallService(
+			new RandomNumbersGenerator(),
+			new PlayerNumbersValidator()
+		);
+		final InputView inputView = new InputView();
+		final OutputView outputView = new OutputView();
+		final BaseBallController baseBallController = new BaseBallController(
+			baseBallService,
+			inputView,
+			outputView
+		);
+
+		baseBallController.run();
+	}
 }
